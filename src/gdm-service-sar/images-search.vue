79cb56fd9c99b0f0dee43cfd09d1a50d4b77f208
epointal <elisabeth.pointal@free.fr>
2024-01-29 10:46:04 +0100

    formulaire de recherche

--- src/gdm-service-sar/images-search.vue
+++ src/gdm-service-sar/images-search.vue
@@ -1,14 +1,33 @@
 <i18n>{
    "fr": {
 	   "acquisition_period": "Période d'acquisition",
+	   "ascending_upper": "ASCENDANT",
 	   "band_upper": "BANDE",
 	   "choose_area_instructions": "Dessiner sur la carte ou saisir une zone géographique en évitant si possible les surfaces aquatiques et la végétation.",
+	   "descending_upper": "DESCENDANT",
+	   "direction_of_orbit_upper": "SENS DE L'ORBITE",
 	   "geographic_area": "Emprise territoriale",
+	   "platform_type_upper": "TYPE DE PLATEFORME",
+	   "polarization_upper": "POLARISATION",
+	   "product_type_upper": "TYPE DE PRODUIT",
 	   "satellite_settings": "Paramètres du satellite",
+	   "sensor_mode_upper": "MODE DU CAPTEUR",
 	   "treatment_level_upper": "NIVEAU DE TRAITEMENT"
    },
    "en": {
-	   
+	   "acquisition_period": "Acquisition period",
+	   "ascending_upper": "ASCENDING",
+	   "band_upper": "BAND",
+	   "choose_area_instructions": "Draw on the map or enter a geographical area avoid, if possible, water surfaces and vegetation.",
+	   "descending_upper": "DESCENDING",
+	   "direction_of_orbit_upper": "DIRECTION OF THE ORBIT",
+	   "geographic_area": "Geographic area",
+	   "platform_type_upper": "PLATEFORM TYPE",
+	   "polarization_upper": "POLARIZATION",
+	   "product_type_upper": "PRODUCT TYPE",
+	   "satellite_settings": "Sattelite settings",
+	   "sensor_mode_upper": "SENSOR MODE",
+	   "treatment_level_upper": "TREATMENT LEVEL"
    }	
 }
 </i18n>
@@ -31,9 +50,9 @@
       <h4 :style="{color:color}" >{{$t('satellite_settings')}}</h4>
       
       <div class="gdm-select">
-        <select v-model="satellite" v-if="service && service.imageProviders"  :style="{backgroundColor:shadeColor}">
+        <select v-model="satellite"  :style="{backgroundColor:shadeColor}">
            <option disabled value="">SATELLITE</option>
-           <option v-for="(imageProvider, key) in service.imageProviders" :value="key" >{{imageProvider.satellite}}</option>
+           <option >SENTINEL-1</option>
         </select>
       </div>
       <div class="gdm-select">
@@ -94,11 +113,16 @@ export default {
 	  color: {
         type: String,
         default: '#191970'
-      },
-      lang: {
-		type: String,
-        default: 'fr'
-	  }
+    },
+    lang: {
+	    type: String,
+      default: 'fr'
+    }
+	},
+	computed: {
+		shadeColor () {
+		   return this.$shadeColor(this.color, 0.8)
+		}
 	},
 	data () {
 		return {
@@ -107,7 +131,6 @@ export default {
 	        end: '',
 	        endDate: null,
 	        defaultTemporal: {start: null, end: null},
-	        email: '',
 	        satellite: 'SENTINEL-1',
 	        instrument: 'SAR-C%20SAR',
 	        treatment: 'LEVEL1',
@@ -120,10 +143,81 @@ export default {
 	        polarizations: ['vv'],
 	        orbitDirection: '',
 	        // orbiteRelative: '',
-	        startIndex: 1,
-
+	        startIndex: 1
 		}
+	},
+	created () {
+		this.$i18n.locale = this.lang
 	}
 	
 }
 </script>
+<style scoped>
+.gdm-select {
+  position:relative;
+  display: inline-block;
+  vertical-align: top;
+  width:304px;
+ }
+.gdm-select select {
+  border: 0;
+  border-radius: 1px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background-color: rgba(172, 220, 238, 0.3);
+  width:calc(100% - 7px);
+}
+.gdm-select::before, .gdm-select::after {
+  content: "";
+  position: absolute;
+  pointer-events: none;
+  box-sizing: border-box;
+}
+.gdm-select::after{
+  content: "\25BC";
+  height: 1em;
+  font-size: .625em;
+  line-height: 1;
+  right: 1.2em;
+  top: 50%;
+  margin-top: -.5em;
+}
+.gdm-select::before {
+  width: 2em;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  border-radius: 0 3px 3px 0;
+}
+.gdm-select option{
+  white-space: initial;
+  -ms-line-break:auto;
+  line-break: auto;
+  word-wrap: break-word;      
+  height: auto;
+  padding: .1em 0;
+  max-width:calc(100% - 30px);
+}
+.gdm-select.disable {
+  pointer-events: none;
+  opacity:0.8;
+}
+
+.gdm-select select {
+  font-size: inherit; 
+  padding: .3em 0 .3em .5em; 
+  margin: 0 0 2px 0; 
+  width: 304px;
+}
+
+.gdm-select select {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background-color: rgba(172,220,238,.3);
+  border: 0;
+  border-radius: 1px;
+  width: calc(100% - 7px);
+}
+</style>
